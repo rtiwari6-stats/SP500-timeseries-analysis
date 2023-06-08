b477c7871184f60a216edd9993cff6e4a94d7e64
genre143 <104961460+genre143@users.noreply.github.com>
2023-06-07 18:01:11 -0700

    Add files via upload
    1. Get data from Y! Finance
    2. Visualize
    3. Convert to log returns
    4. Visualize again.

---
+++ project_spdr_spy.r
@@ -0,0 +1,33 @@
+#626 project (quantmod)
+library(quantmod)
+library(ggplot2)
+library(ggfortify)
+
+
+#pull data
+getSymbols("SPY", src = 'yahoo', 
+                 from = "1993-01-29", to = "2023-06-07") # Note this can be current date too
+#XTS object!
+str(SPY)
+
+#print some data
+head(SPY)
+
+#plot s&p 500 series
+autoplot(SPY$SPY.Adjusted, ts.colour = "dodgerblue3", main = "SPY data from 1993-01-29 to 2023-06-07") + 
+  labs(x = "Observation Date", y = "SPY daily adj. closing price") 
+
+#create log returns
+#step 1 convert to a data frame (can be tibble too)
+
+spy_df = data.frame(date=index(SPY), coredata(SPY))
+#show some data
+head(spy_df)
+#plot s&p 500 series
+ggplot(data = spy_df, mapping = aes(x = date, y = SPY.Adjusted)) +
+  geom_line() +
+  labs(x = "Observation Date", y = "SPY daily log returns") + 
+  ggtitle("S&P 500 log returns from 1993-01-29 to 2023-06-07")
+
+
+
