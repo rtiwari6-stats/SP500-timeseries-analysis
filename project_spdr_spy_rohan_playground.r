14f8102cda3d96d0480feaef2c83da71fa453e8d
genre143 <104961460+genre143@users.noreply.github.com>
2023-06-16 18:45:29 -0700

    Update project_spdr_spy_rohan_playground.r
    lowess smoother
    arima, acf, pacf
    
    Still can't get the labels working directly with xts for trend plots

--- project_spdr_spy_rohan_playground.r
+++ project_spdr_spy_rohan_playground.r
@@ -7,6 +7,8 @@ library(grid)
 library(forecast)
 library(tseries)
 library(seastests)
+library(TSstudio)
+library(astsa)
 #pull data
 getSymbols("SPY", src = 'yahoo', 
            from = "1993-01-29", to = "2023-06-07") # Note this can be current date too
@@ -53,7 +55,7 @@ SPY_quarterly = to.quarterly(SPY) #convert the OHLC series to quarterly
 plot_all_spy_series(SPY_quarterly, title="SPY Quarterly data from 1993-01-29 to 2023-06-07", 
                     nRow=ncol(SPY_quarterly)-1, date_breaks = '3 months', date_labels = '%b %Y', angle=90)
 
-#check for seasonality
+#check for seasonality using decomposition
 #let us write a decomposed series plotter first
 decomp_func <- function(dataset, nRow = 3, title = "Decomposition Plots for SPY") {
 
@@ -105,10 +107,20 @@ decomp_func(spy_monthly_ts, title="Decomposition Plots for Monthly SPY")
 spy_quarterly_ts = ts(data = coredata(SPY_quarterly), start = c(1993,1), end = c(2023,6), frequency = 4)
 decomp_func(spy_quarterly_ts, title="Decomposition Plots for Quarterly SPY")
 
+#create log returns on adjusted price
+#note that this is a random walk because we take the difference x(t) - x(t-1) as the first step to compute the returns
+# whatever is left is supposed to be random error.
+#step 1 convert to a data frame (can be tibble too)
+#this is daily series, so we have daily returns
+SPY$logDiff.Adjusted = diff(log(SPY$SPY.Adjusted))
+
+#plot log s&p 500 series
+autoplot(SPY$logDiff.Adjusted, ts.colour = "dodgerblue3", main = "SPY log returns from 1993-01-29 to 2023-06-07", 
+         xlab = "Observation Date", ylab = "SPY daily adj. log returns") 
 
 #we see an obvious increasing trend in the adjusted closing process. We try a trend-only model to SPY
-#diagnostic plots have been commented out to minimize the number of plots generated
-spy_ts = as.ts(SPY)
+#diagnostic plots have been commented out to minimize the number of plots generated. This trend only model should show the direction of the trend, we expect a horizontal line for the log diff series.
+spy_ts = na.omit(as.ts(SPY[, c("SPY.Open", "SPY.High", "SPY.Low", "SPY.Close", "SPY.Volume", "SPY.Adjusted", "logDiff.Adjusted")]))
 trend = time(spy_ts)
 plot_trend = function(ts, line){
   plot(spy_ts[,ts], main = paste("Linear trend for", ts))
@@ -116,7 +128,7 @@ plot_trend = function(ts, line){
 }
 
 par(mar=c(1.5,1.5,1.5,1.5))
-par(mfrow=c(3,2))
+par(mfrow=c(3,3))
 #TODO: axis naming and scales
 for(i in 1:ncol(spy_ts)){
   m = lm(spy_ts[,i] ~ trend, na.action = NULL)
@@ -124,21 +136,49 @@ for(i in 1:ncol(spy_ts)){
 }
 
 #try quadratic trend for adjusted price
+#TODO: axis naming and scales
 plot_quad_trend = function(ts, curve){
   pred = predict(curve)
   ix = base::sort(trend, index.return=T)
-  plot(spy_ts[,ts], main = paste("Quad Linear trend for", ts))
+  plot(spy_ts[,ts], main = paste("Quad trend for", ts))
   lines(trend[ix], pred[ix],lwd=2, col="blue")
 }
 par(mar=c(1.5,1.5,1.5,1.5))
-par(mfrow=c(3,2))
+par(mfrow=c(3,3))
 #TODO: axis naming and scales
 for(i in 1:ncol(spy_ts)){
   m = lm(spy_ts[,i] ~ trend + I(trend ^ 2), na.action = NULL)
   plot_quad_trend(colnames(spy_ts)[i], m)
 }
 
+#lowess smoother with different spans
+plot_lowess_smoother = function(ts){
+  m1 = lowess(spy_ts[, ts], f=0.5)
+  m2 = lowess(spy_ts[, ts], f=0.05)
+  m3 = lowess(spy_ts[, ts], f=0.005)
+  m4 = lowess(spy_ts[, ts], f=0.0005)
+  m5 = lowess(spy_ts[, ts], f=0.00005)
+  plot(spy_ts[,ts], main = paste("Lowess smoothing with different spans for", ts))
+  lines(m1, lwd=2, col="red")
+  lines(m2, lwd=2, col="blue")
+  lines(m3, lwd=2, col="green")
+  lines(m3, lwd=2, col="lightblue")
+  lines(m3, lwd=2, col="purple")
+  legend("topleft",                                              
+         col = c("red", "blue", "green", 'lightblue', "purple"),
+         lty = 1,
+         lwd = 2,
+         c("0.5", "0.05", "0.005", "0.0005", "0.00005"))
+}
+par(mar=c(1.5,1.5,1.5,1.5))
+par(mfrow=c(3,3))
+#TODO: Please fix naming and scales
+for(i in 1:ncol(spy_ts)){
+  plot_lowess_smoother(colnames(spy_ts)[i])
+} #the one with smallest span captures the series well which is expected
+
 #try splines for adjusted price
+#TODO: axis naming and scales
 spline_trend=function(ts, gam1, gam2, gam3){
   ord <- order(trend)
   plot(spy_ts[,ts], main = paste("Non Linear trend for", ts))
@@ -149,34 +189,26 @@ spline_trend=function(ts, gam1, gam2, gam3){
 
 library(mgcv)
 par(mar=c(1.5,1.5,1.5,1.5))
-par(mfrow=c(3,2))
+par(mfrow=c(3,3))
 #TODO: axis naming and scales
 for(i in 1:ncol(spy_ts)){
-  gam_4 = mgcv::gam(spy_ts[,"SPY.Adjusted"]~s(trend, k=10,bs="cr"))
-  gam_25 = mgcv::gam(spy_ts[,"SPY.Adjusted"]~s(trend, k=25,bs="cr"))
-  gam_200 = mgcv::gam(spy_ts[,"SPY.Adjusted"]~s(trend, k=200,bs="cr"))
+  gam_4 = mgcv::gam(spy_ts[,i]~s(trend, k=10,bs="cr"))
+  gam_25 = mgcv::gam(spy_ts[,i]~s(trend, k=25,bs="cr"))
+  gam_200 = mgcv::gam(spy_ts[,i]~s(trend, k=200,bs="cr"))
   spline_trend(colnames(spy_ts)[i], gam_4, gam_25, gam_200)
 }# still not sufficient, we need need a large number of splines to capture the fluctuations in the trend!
-
-#try boxcox
-spy_adjusted_bc = BoxCox(spy_ts[,"SPY.Adjusted"], lambda = BoxCox.lambda(spy_ts[,"SPY.Adjusted"]))
-trend_spy_bc = lm(spy_adjusted_bc
-                  ~ trend, na.action = NULL)
-summary(trend_spy_bc)
-#plot(trend_spy_bc) #doesn't look good
-plot(spy_adjusted_bc)
 
 par(mar=c(5,4,4,1))
 par(mfrow=c(1,1))
 
-#create log returns on adjusted price
-#step 1 convert to a data frame (can be tibble too)
-#this is daily series, so we have daily returns
-SPY$logDiff.Adjusted = diff(log(SPY$SPY.Adjusted))
+#try boxcox
 
-#plot log s&p 500 series
-autoplot(SPY$logDiff.Adjusted, ts.colour = "dodgerblue3", main = "SPY log returns from 1993-01-29 to 2023-06-07", 
-         xlab = "Observation Date", ylab = "SPY daily adj. log returns") 
+#spy_adjusted_bc = BoxCox(spy_ts[,"SPY.Adjusted"], lambda = BoxCox.lambda(spy_ts[,"SPY.Adjusted"]))
+#trend_spy_bc = lm(spy_adjusted_bc
+                  #~ trend, na.action = NULL)
+#summary(trend_spy_bc)
+#plot(trend_spy_bc) #doesn't look good
+#plot(spy_adjusted_bc)
 
 #let's try adf test on logged and original
 adf.test(na.omit(SPY$logDiff.Adjusted)) # null hypothesis rejected. STATIONARY!
@@ -192,22 +224,60 @@ isSeasonal(SPY$logDiff.Adjusted, freq = 365) # returns False!
 stl(na.omit(SPY$logDiff.Adjusted)) # doesn't work, so not seasonal
 
 #autocorrelation
-acf(SPY$logDiff.Adjusted, na.action = na.pass, main="DiifLog SPY returns 1993-01-29 to 2023-06-07") #almost white noise except perhaps at lag 1?
-pacf(SPY$logDiff.Adjusted, na.action = na.pass, main="DiifLog SPY returns 1993-01-29 to 2023-06-07") # some significant values and a tail-off?
-
+acf(SPY$logDiff.Adjusted, na.action = na.pass, 
+    main="DiifLog SPY returns 1993-01-29 to 2023-06-07") #almost white noise?
+pacf(SPY$logDiff.Adjusted, na.action = na.pass, 
+     main="DiifLog SPY returns 1993-01-29 to 2023-06-07") # some significant values and a tail-off?
 
 #split into train and test
-train_date <- nrow(SPY) *0.8 #80% train
+train_date = nrow(SPY) *0.8 #80% train
 spy_train = SPY[1:train_date,]
-spy_test <- SPY[-c(1:train_date),] #20% test
+spy_test = SPY[-c(1:train_date),] #20% test
 tail(spy_train) # ends at 2017-05-09
 head(spy_test) # starts at 2017-05-10 - so the split works
 
 #arima for daily returns
-spy.arima = auto.arima(na.omit(spy_train$logDiff.Adjusted)) 
-summary(spy.arima) #ARIMA(1,0,1) with non-zero mean 
-spy.forecast = forecast(spy.arima, h=length(spy_test))
-accuracy(spy.forecast, spy_test$logDiff.Adjusted) #RMSE on test is 0.01269035
-autoplot(spy.forecast, ylab="Log Diff Adjusted Price", 
+#pick the one with lowest AIC and BIC
+spy.arima.adj.price = auto.arima(na.omit(spy_train$logDiff.Adjusted)) 
+summary(spy.arima.adj.price) #ARIMA(1,0,1) with non-zero mean 
+spy.forecast.adj.price = forecast(spy.arima.adj.price, h=length(spy_test$logDiff.Adjusted))
+accuracy(spy.forecast.adj.price, spy_test$logDiff.Adjusted) #RMSE on test is 0.01269035
+autoplot(spy.forecast.adj.price, ylab="Log Diff Adjusted Price", 
          main="SPY Log Diff Adjusted Price forecasts 2017-05-09 onwards")
+#lets plot everything
+test_forecast(actual=na.omit(SPY$logDiff.Adjusted), forecast.obj = spy.forecast.adj.price, 
+              test=na.omit(spy_test$logDiff.Adjusted))
+#residuals from arima
+checkresiduals(spy.arima.adj.price) # reasonable but can be better. Not normal, some acfs are significant, heterskedasticity
 
+#more arima
+# Use acf and pacf:
+#for AR models, the ACF will decay quickly and the PACF plot will be used to identify the order (p) of the AR model. For MA models, 
+#the PACF will decay quickly and the ACF plot will be used to identify the order (q) of the MA model
+#looking at PACF we pick p=1
+#looking at ACF we pick q= 1 and 15
+# we keep d=0,1
+#we try arima(1,1,15) (1,1,16), (36,1,15) and (36,1, 15). R
+for(p in c(1)){
+  for(q in c(1,15)){
+    for(d in c(0,1)){
+      spy.candidate.arima.adj.price = arima(na.omit(spy_train$logDiff.Adjusted), order=c(p,d,q))
+      spy.candidate.forecast.adj.price = forecast(spy.candidate.arima.adj.price, h=length(spy_test$logDiff.Adjusted))
+      #autoplot(spy.candidate.forecast.adj.price, ylab="Log Diff Adjusted Price", 
+      #         main=paste("SPY Log Diff Adjusted Price forecasts 2017-05-09 onwards for arima", p,d,q, ")"))
+      print(paste("Accuracy for arima(", p,d,q, ")"))
+      print(accuracy(spy.candidate.forecast.adj.price, spy_test$logDiff.Adjusted))
+    }
+  }
+}# (1,0,1) and (1,1,1) are both producing the lowest RMSE on the test set. So auto.arima matches acf/pacf observations.
+
+
+#next bring in SPY.Volume to see if something is useful
+#ccf needs numeric vectors as inputs!
+#Note these are daily data
+# Also note: I am not sure if using volume column even makes sense
+ccf = ccf(as.numeric(SPY$SPY.Volume), as.numeric(na.omit(SPY$logDiff.Adjusted)),
+    main="CCF between SPY Volume and LogDiffAdjustedPrices")
+print(ccf) #lags -> 1, 2, 3, 4, 5 till 24 seem interesting?
+#let us look at lag plots
+lag2.plot(as.numeric(SPY$SPY.Volume), as.numeric(na.omit(SPY$logDiff.Adjusted)),5)
